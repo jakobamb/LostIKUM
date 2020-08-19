2cd701a422ae1a21f955d718eb29ae49b1ac3e99
Timmisch <18immisch@infomatik.uni-hamburg.de>
2020-08-19 14:06:49 +0200

    Untracked Skripts geadded

---
+++ Assets/Our scripts/SchlossCabinet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SchlossCabinet : MonoBehaviour
+{
+    public GameObject drawer;
+    ConfigurableJoint drawerslide;
+    public bool locked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Lock();
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {   
+        if(other.gameObject.CompareTag("CabinetKey"))
+        {
+            Unlock();
+        }
+    }
+    
+    private void Lock()
+    {
+        drawerslide = drawer.GetComponent<ConfigurableJoint>();
+        drawerslide.xMotion = ConfigurableJointMotion.Locked;
+        locked = true;
+    }
+    
+    private void Unlock()
+    {
+        drawerslide = drawer.GetComponent<ConfigurableJoint>();
+        drawerslide.xMotion = ConfigurableJointMotion.Limited;
+        locked = false;
+    }
+}
