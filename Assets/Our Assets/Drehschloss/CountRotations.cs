71c529e7514e9f6a8f9eca95593913fd5f45e541
Jakob Ambsdorf <jakob.ambsdorf@gmail.com>
2020-09-15 15:02:47 +0200

    refactored Drehschloss code. Registers correct combination and unlocks cabinet

--- Assets/Our Assets/Drehschloss/CountRotations.cs
+++ Assets/Our Assets/Drehschloss/CountRotations.cs
@@ -9,13 +9,11 @@ public class CountRotations : MonoBehaviour
     public UnityEvent onFullRotation;
     public UnityEvent onUnlock;
 
-    public List<int> CORRECT_COMBI = new List<int>() { 1, -1, 1};
+    public List<int> CORRECT_COMBI = new List<int>() { 1, -1, 1 };
+    public List<int> _currentCombination = new List<int>();
 
     public SchrankTransitionMgr schrank;
 
-    private CurrentCombination combination = new CurrentCombination();   
-
-    private bool _isSelected = false;
     private float _cumulatedRotation = 0.0f; 
     private float _lastFrameEulerRotation = 0f;
 
@@ -60,74 +58,49 @@ public class CountRotations : MonoBehaviour
         if (_cumulatedRotation > 360)
         {
             //add +1 to list -> complete right turn
-            combination.addCombiValue(+1);
+            _currentCombination.Add(+1);
             Debug.Log("[CountRotations] 360 Grad nach rechts gedreht");
         }
         else if (_cumulatedRotation < -360)
         {
             //add -1 to list -> complete left turn
-            combination.addCombiValue(-1);
+            _currentCombination.Add(-1);
             Debug.Log("[CountRotations] 360 Grad nach links gedreht");
-
         }
         // reset the button
         _cumulatedRotation = 0;
         transform.localEulerAngles = new Vector3(0, 0, 0);
         _lastFrameEulerRotation = 0;
 
-        //_lastFrameEulerRotation = 0f;
+        // check if the combination is correct
+        if (isCombiCorrect())
+        {
+            correctCombiHandler();
+            return;
+        }
 
         // StartCoroutine(disableRotation());
         onFullRotation.Invoke();
     }
 
-
-    //called by trigger360degree
-    //freezes the button for a second
-    //calls correctCombiHandler if Combi is correct
-    //plays sound
-    // What is this for???
-    IEnumerator disableRotation(){
-        
-        HingeJoint hinge = GetComponent<HingeJoint>();
-        JointLimits limits = hinge.limits;
-
-        hinge.useLimits = true;
-        limits.min = 0;
-        limits.max = 0;
-        hinge.limits = limits;
-
-
-         //checks if combi is correct
-        if(isCombiCorrect()){
-            correctCombiHandler();
-            yield return new WaitForSeconds(1);
-            hinge.useLimits = false;
-        } else {
-            yield return new WaitForSeconds(1);
-            hinge.useLimits = false;
-        }
-    }
-
-
     //checks if the current combination is correct
     private bool isCombiCorrect() {
-        List<int> currentCombination = combination.getCurrentCombination();
-        bool isCombiCorrect = false;
- 
-        if(currentCombination.Count >= CORRECT_COMBI.Count) {
-            //prüft paarweise, ob die Listen übereinstimmen
-            for (int i = 0; i < CORRECT_COMBI.Count-1; i++){
-                if(!(CORRECT_COMBI[i] == currentCombination[currentCombination.Count-CORRECT_COMBI.Count+i])) {
-                    //gibt false zurück, falls Listen an einer Stelle nicht übereinstimmen
-                    return false;
-                }
+        
+        for (int i = 0; i < _currentCombination.Count; i++) {
+            if (_currentCombination[i] != CORRECT_COMBI[i])
+            {
+                _currentCombination.Clear();
+                return false;
             }
-            
-            //wird auf true gesetzt, nachdem die Schleife fertig gelaufen ist.
-            isCombiCorrect=true;
         }
-        return isCombiCorrect;
+
+        if (_currentCombination.Count == CORRECT_COMBI.Count)
+        {
+            // Both lists have been successfully traversed.
+            return true;
+        }
+
+        return false;
     }
 
     //handles the correct combination
@@ -135,8 +108,7 @@ public class CountRotations : MonoBehaviour
 
         // tell the Transition Manager
         schrank.DrehschlossUnlocked();
-
         Debug.Log("Correct Combination!");
-        combination.clearCombi();
+        onUnlock.Invoke();
     }
 }
