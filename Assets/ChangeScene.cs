5f3532e30fdcb3da134d5674098afd29be15ca5a
Nadibu <nadibu18@gmail.com>
2020-09-23 13:41:02 +0200

    Naja... das Intro.

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+
+public class ChangeScene : MonoBehaviour
+{
+    public VideoPlayer VideoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!VideoPlayer.isPlaying)
+        {
+            SceneManager.LoadScene(sceneName: "Main scene");
+        }
+    }
+}
